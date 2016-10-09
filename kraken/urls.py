f4344aa3a074bcae12aae91cf35e42f3f8fb8d93
Donny Davis <donny@garage.fortnebula.com>
2016-10-09 13:05:42 -0400

    cleaning the up the S3 Mess

--- kraken/urls.py
+++ kraken/urls.py
@@ -4,15 +4,11 @@ from status.views import home
 from status.views import api
 from status.views import activity
 from status.views import osd_details
-from ops.views import ops
-from ops.views import user_custom
 
 urlpatterns = [
     url(r'^$', home),
-    url(r'^ops/$', ops),
     url(r'^api/$', api),
     url(r'^osd/(\d+)/$', osd_details),
     url(r'^activity/$', activity),
-    url(r'^user/(.+?)(/.+?)?(/.+?)?/$', user_custom),
     
 ]
